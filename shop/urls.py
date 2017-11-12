a32bcd04870d56ea9f4e27abdc7686ac52db0454
Edvin Sladic <sladic.edvin@gmail.com>
2017-11-12 17:05:26 +0100

    Added view for '/shop/category/'. It was previously 404.

--- shop/urls.py
+++ shop/urls.py
@@ -5,5 +5,6 @@ from shop import views
 urlpatterns = [
     url(r'^$', views.Overview.as_view(), name='shop'),
     url(r'product/(?P<pk>[0-9]+)/$', views.ProductView.as_view(), name='shop_detail'),
+    url(r'category/$', views.CategoryOverview.as_view(), name='shop_category_overview'),
     url(r'category/(?P<pk>[0-9]+)/$', views.CategoryView.as_view(), name='shop_category'),
 ]
