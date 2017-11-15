c99a3e15c3c263d0fcff3233cceb9679b8c7c1f5
SpyTec <spytec13@gmail.com>
2017-11-15 16:11:55 +0100

    Updated migrations. Increased tests

--- shop/tests.py
+++ shop/tests.py
@@ -3,63 +3,66 @@ from .models import Category, Product
 
 
 class RootTests(TestCase):
-    def test_shop_root(self):
+    def test_shop_root_redirects(self):
         client = Client()
         response = client.get('/shop')
         self.assertEqual(response.status_code, 301)
 
-    def test_shop_root_(self):
+    def test_shop_root_shows(self):
         client = Client()
         response = client.get('/shop/')
         self.assertEqual(response.status_code, 200)
 
-    def test_category_root(self):
+    def test_category_root_redirects(self):
         client = Client()
         response = client.get('/shop/category')
         # Maybe we should ad an redirect on this path to '/shop/category/'
         self.assertEqual(response.status_code, 301)
 
-    def test_category_root_(self):
+    def test_category_root_shows(self):
         client = Client()
         response = client.get('/shop/category/')
         # I think we should make this page
         self.assertEqual(response.status_code, 200)
 
-    def test_category(self):
+    def test_category_redirects(self):
         client = Client()
         response = client.get('/shop/category/1')
         self.assertEqual(response.status_code, 301)
 
-    def test_category_(self):
-        category = Category(name='CPU')
-        category.save()
+    def test_category_displays_specific_category(self):
+        product = Product(name='i7', description="Bla bla bla", price=200, inventory=3)
+        product.save()
+        product.categories.create(name='CPU')
         client = Client()
         response = client.get('/shop/category/1/')
         self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "i7")
 
     def test_category_nonExistingCategory(self):
         client = Client()
         response = client.get('/shop/category/non/')
         self.assertEqual(response.status_code, 404)
 
-    def test_product_root(self):
+    def test_product_root_page_redirects(self):
         client = Client()
         response = client.get('/shop/product')
         self.assertEqual(response.status_code, 404)
 
-    def test_product_root_(self):
+    def test_product_root_page(self):
         client = Client()
         response = client.get('/shop/product/')
         self.assertEqual(response.status_code, 404)
 
-    def test_product(self):
+    def test_product_page_redirects(self):
         client = Client()
         response = client.get('/shop/product/1')
         self.assertEqual(response.status_code, 301)
 
-    def test_product_(self):
-        product = Product(name='i7', description="Bla bla bla", price=200)
+    def test_product_is_being_shown(self):
+        product = Product(name='i7', description="Bla bla bla", price=200, inventory=3)
         product.save()
         client = Client()
         response = client.get('/shop/product/1/')
         self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Bla bla bla")
